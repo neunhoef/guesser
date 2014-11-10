5b707ffba25e0b98e8ad70e1828598b59bacf6ea
Max Neunhoeffer <m.neunhoeffer@triagens.de>
2014-11-09 20:02:22 -0500

    First working version.

--- guesser.js
+++ guesser.js
@@ -8,13 +8,15 @@
   "use strict";
   var Foxx = require("org/arangodb/foxx"),
       ArangoError = require("org/arangodb").ArangoError,
+      log = require("console").log,
       controller = new Foxx.Controller(applicationContext);
 
+  var collName = applicationContext.collectionName("questions");
   var coll = applicationContext.collection("questions");
 
   // Get all current entries:
   controller.get('/get/:key', function (req, res) {
-    require("console").log("get/"+req.urlParameters["key"]+" called");
+    log("get/"+req.urlParameters["key"]+" called");
     var d;
     try {
       d = coll.document(req.urlParameters["key"]);
@@ -23,6 +25,49 @@
     catch (e) {
       res.json(e);
     }
+  });
+
+  // Post a new question and thingy:
+  controller.put('/put', function (req, res) {
+    log("put called");
+    var db = require("internal").db;
+    var b = req.body();
+    try {
+      db._executeTransaction( {
+        collections: {
+          write: [collName]
+        },
+        action: function () {
+          var oldLeaf = coll.document(b.oldLeaf);
+          log(JSON.stringify(oldLeaf));
+          var oldParent = coll.document(oldLeaf.parent);
+          log(JSON.stringify(oldParent));
+          b.newQuestion.parent = oldLeaf.parent;
+          var newQuestion = coll.insert(b.newQuestion);
+          log(JSON.stringify(newQuestion));
+          b.newLeaf.parent = newQuestion._key;
+          var newLeaf = coll.insert(b.newLeaf);
+          log(JSON.stringify(newLeaf));
+          coll.update(newQuestion._key, { goto2: newLeaf._key });
+          coll.update(oldLeaf._key, {parent: newQuestion._key});
+          log("Updates OK");
+          if (oldParent.goto1 === b.oldLeaf) {
+            coll.update(oldParent._key, { goto1: newQuestion._key });
+          }
+          else if (oldParent.goto2 === b.oldLeaf) {
+            coll.update(oldParent._key, { goto2: newQuestion._key });
+          }
+          else {
+            throw "Murks";
+          }
+        },
+      });
+    }
+    catch (e) {
+      res.json(e);
+      return;
+    }
+    res.json({"error":false});
   });
 
 }());
--- scripts/setup.js
+++ scripts/setup.js
@@ -5,7 +5,15 @@
       collname = applicationContext.collectionName("questions");
 
   if (db._collection(collname) === null) {
-    db._create(collname);
+    var c = db._create(collname);
+    var q = c.insert({ isLeaf: false, question: "Is it a thing or an animal?",
+                       answer1: "a thing", answer2: "an animal",
+                       goto1: "computer", goto2: "cat", _key: "root",
+                       parent: null});
+    c.insert({ isLeaf: true, guess: "a computer", 
+               parent: q._key, _key: "computer" });
+    c.insert({ isLeaf: true, guess: "a cat", 
+               parent: q._key, _key: "cat" });
   } else if (applicationContext.isProduction) {
     console.warn("collection '%s' already exists. Leaving it untouched.", collname);
   }
