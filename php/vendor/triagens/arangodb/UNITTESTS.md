0c841a0bbb1b00c835129ef1fc7e4c499d186efe
Max Neunhoeffer <m.neunhoeffer@triagens.de>
2014-11-13 16:33:15 -0500

    Remove dependencies for php.

--- php/vendor/triagens/arangodb/UNITTESTS.md
+++
@@ -1,8 +0,0 @@
-# PHPUnit Tests for ArangoDB-PHP
-
-To run the unit tests, cd into the tests folder and run:
-
-<code>
-phpunit --testsuite ArangoDB-PHP
-</code>
-
--- php/vendor/triagens/arangodb/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace triagens\ArangoDb;
-
-require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'triagens' . DIRECTORY_SEPARATOR . 'ArangoDb' . DIRECTORY_SEPARATOR . 'Autoloader.php';
-
-Autoloader::init();
